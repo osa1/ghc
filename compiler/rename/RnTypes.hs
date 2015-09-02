bef705916f7856973283f23641886c7388ef0725
Johan Tibell <johan.tibell@gmail.com>
2015-09-02 08:14:25 -0700

    Add unboxed sums

--- compiler/rename/RnTypes.hs
+++ compiler/rename/RnTypes.hs
@@ -543,6 +543,11 @@ rnHsTyKi env tupleTy@(HsTupleTy tup_con tys)
        ; (tys', fvs) <- mapFvRn (rnLHsTyKi env) tys
        ; return (HsTupleTy tup_con tys', fvs) }
 
+rnHsTyKi what doc (HsUSumTy tys)
+  = do { unless (isRnType what) (addErr (text "Unboxed sum kinds not supported"))
+       ; (tys', fvs) <- mapFvRn (rnLHsTyKi what doc) tys
+       ; return (HsUSumTy tys', fvs) }
+
 -- Ensure that a type-level integer is nonnegative (#8306, #8412)
 rnHsTyKi env tyLit@(HsTyLit t)
   = do { data_kinds <- xoptM LangExt.DataKinds
@@ -1592,6 +1597,7 @@ extract_lty t_or_k (L _ ty) acc
       HsListTy ty               -> extract_lty t_or_k ty acc
       HsPArrTy ty               -> extract_lty t_or_k ty acc
       HsTupleTy _ tys           -> extract_ltys t_or_k tys acc
+      HsUSumTy tys              -> extract_ltys t_or_k tys acc
       HsFunTy ty1 ty2           -> extract_lty t_or_k ty1 =<<
                                    extract_lty t_or_k ty2 acc
       HsIParamTy _ ty           -> extract_lty t_or_k ty acc
