f9e6c6be41edf37e336fe114a98c7a2fd9a55c4a
Richard Eisenberg <eir@cis.upenn.edu>
2015-09-02 13:51:16 -0700

    Renaming/typechecking/desugaring unboxed sums

--- compiler/typecheck/TcExpr.hs
+++ compiler/typecheck/TcExpr.hs
@@ -467,6 +467,12 @@ tcExpr expr@(ExplicitTuple tup_args boxity) res_ty
 
        ; return $ mkHsWrap wrap (ExplicitTuple tup_args1 boxity) }
 
+tcExpr (HsSum selector arity expr _) res_ty
+  = do { let sum_tc = sumTyCon arity
+       ; (coi, arg_tys) <- matchExpectedTyConApp sum_tc res_ty
+       ; expr' <- tcMonoExpr expr (arg_tys `getNth` selector)
+       ; return $ mkHsWrapCo coi (HsSum selector arity expr' arg_tys) }
+
 tcExpr (ExplicitList _ witness exprs) res_ty
   = case witness of
       Nothing   -> do  { (coi, elt_ty) <- matchExpectedListTy res_ty
