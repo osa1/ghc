47fcaecd206c152779beb57e54da653f466503d0
Johan Tibell <johan.tibell@gmail.com>
2015-09-03 08:37:43 -0700

    Add a DynFlag to enable unboxed sums

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -3280,6 +3280,7 @@ xFlags = [
   flagSpec "TypeOperators"                    LangExt.TypeOperators,
   flagSpec "TypeSynonymInstances"             LangExt.TypeSynonymInstances,
   flagSpec "UnboxedTuples"                    LangExt.UnboxedTuples,
+  flagSpec "UnboxedSums"                      LangExt.UnboxedSums,
   flagSpec "UndecidableInstances"             LangExt.UndecidableInstances,
   flagSpec "UndecidableSuperClasses"          LangExt.UndecidableSuperClasses,
   flagSpec "UnicodeSyntax"                    LangExt.UnicodeSyntax,
@@ -3578,6 +3579,7 @@ glasgowExtsFlags = [
            , LangExt.TypeOperators
            , LangExt.TypeSynonymInstances
            , LangExt.UnboxedTuples
+           , LangExt.UnboxedSums
            , LangExt.UnicodeSyntax
            , LangExt.UnliftedFFITypes ]
 
