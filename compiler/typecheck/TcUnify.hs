1b3f4046601baf13daa061c33152642208c48d60
Johan Tibell <johan.tibell@gmail.com>
2015-09-04 10:48:25 -0700

    Hopefully finish the pattern typing story
    Conflicts:
    	compiler/deSugar/Check.hs
    	compiler/prelude/TysWiredIn.hs

--- compiler/typecheck/TcUnify.hs
+++ compiler/typecheck/TcUnify.hs
@@ -28,7 +28,6 @@ module TcUnify (
   matchExpectedFunTys,
 
   matchExpectedFunKind,
-  matchExpectedSumTy,
   wrapFunResCoercion
 
   ) where
@@ -58,7 +57,6 @@ import Bag
 import Util
 import Outputable
 import FastString
-import ListSetOps ( getNth )
 
 import Control.Monad
 
@@ -217,14 +215,6 @@ matchExpectedListTy :: TcRhoType -> TcM (TcCoercionN, TcRhoType)
 matchExpectedListTy exp_ty
  = do { (co, [elt_ty]) <- matchExpectedTyConApp listTyCon exp_ty
       ; return (co, elt_ty) }
-
-----------------------
-matchExpectedSumTy :: Int -> Int -> TcRhoType -> TcM (TcCoercionN, TcRhoType)
--- Special case for sums
-matchExpectedSumTy alt arity exp_ty
- = do { let tc = sumTyCon arity
-      ; (coi, arg_tys) <- matchExpectedTyConApp tc exp_ty
-      ; return (coi, arg_tys `getNth` alt) }
 
 ----------------------
 matchExpectedPArrTy :: TcRhoType -> TcM (TcCoercionN, TcRhoType)
