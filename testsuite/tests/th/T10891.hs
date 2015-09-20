7305e66d3c351e21af14ff0752d6b4a4f51ab061
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-09-20 16:09:54 -0400

    fix test

--- testsuite/tests/th/T10891.hs
+++ testsuite/tests/th/T10891.hs
@@ -1,11 +1,37 @@
-module Main where
+{-# LANGUAGE TypeFamilies #-}
 
-import T10891_Classes
+module T10891 where
 
 import Language.Haskell.TH
+import System.IO
 
-main :: IO ()
-main = do
-  putStrLn $(stringE . show =<< reify ''C)
-  putStrLn $(stringE . show =<< reify ''C')
-  putStrLn $(stringE . show =<< reify ''C'')
+class C a where
+  f :: a -> Int
+
+class C' a where
+  type F a :: *
+  type F a = a
+  f' :: a -> Int
+
+class C'' a where
+  data Fd a :: *
+
+instance C' Int where
+  type F Int = Bool
+  f' = id
+
+instance C'' Int where
+  data Fd Int = B Bool | C Char
+
+$(return [])
+
+test :: ()
+test =
+  $(let
+      display :: Name -> Q ()
+      display q = do { i <- reify q; runIO $ hPutStrLn stderr (pprint i) }
+    in do
+      display ''C
+      display ''C'
+      display ''C''
+      [| () |])
