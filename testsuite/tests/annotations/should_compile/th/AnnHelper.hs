098b2c6c4418c043874e908c95960a26a0c953e4
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-10-19 10:22:21 -0400

    update a test

--- testsuite/tests/annotations/should_compile/th/AnnHelper.hs
+++ testsuite/tests/annotations/should_compile/th/AnnHelper.hs
@@ -5,12 +5,12 @@ import Language.Haskell.TH.Syntax
 
 traverseModuleAnnotations :: Q [String]
 traverseModuleAnnotations = do
-  ModuleInfo children <- reifyModule =<< thisModule
+  ModuleInfo _currentMod children <- reifyModule =<< thisModule
   go children [] []
   where
     go []     _visited acc = return acc
     go (x:xs) visited  acc | x `elem` visited = go xs visited acc
                            | otherwise = do
-                             ModuleInfo newMods <- reifyModule x
+                             ModuleInfo _mod newMods <- reifyModule x
                              newAnns <- reifyAnnotations $ AnnLookupModule x
                              go (newMods ++ xs) (x:visited) (newAnns ++ acc)
