9b6a5e19e83f9395e6e1a0d5d3eaabd870f95e64
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-12-01 13:51:44 -0500

    Implement UnboxedSums
    Summary:
    This implements UnboxedSums as described in
    https://ghc.haskell.org/trac/ghc/wiki/UnpackedSumTypes
    
    We don't implement UNPACKing sum types here, it's implemented in D1540, but
    needs updates after this one is merged.
    
    This is WIP - submitting to Phab for reviews and comments.
    
    Test Plan: Get reviews, complete implementation, write tests.
    
    Reviewers: austin, goldfire, bgamari
    
    Subscribers: goldfire, thomie, tibbe, rrnewton, mpickering
    
    Differential Revision: https://phabricator.haskell.org/D1559

---
+++ compiler/simplStg/ElimUbxSums.hs
@@ -0,0 +1,171 @@
+{-# LANGUAGE CPP, TupleSections #-}
+
+module ElimUbxSums ( elimUbxSums ) where
+
+#include "HsVersions.h"
+
+import BasicTypes (Boxity (..))
+import CoreSyn (AltCon (..))
+import DataCon
+import FastString (mkFastString)
+import Id (idType, mkSysLocalM)
+import Literal (Literal (..))
+import Outputable
+import StgSyn
+import TyCon
+import Type
+import TysPrim (anyTypeOfKind, intPrimTy)
+import TysWiredIn (tupleDataCon)
+import UniqSupply
+
+import Control.Monad (replicateM)
+import Data.List (partition)
+
+--------------------------------------------------------------------------------
+
+elimUbxSums :: [StgBinding] -> UniqSM [StgBinding]
+elimUbxSums = mapM elimUbxSum
+
+elimUbxSum :: StgBinding -> UniqSM StgBinding
+elimUbxSum (StgNonRec bndr rhs)
+  = StgNonRec bndr <$> elimUbxSumRhs rhs
+elimUbxSum (StgRec bndrs)
+  = StgRec <$> mapM (\(bndr, rhs) -> (bndr,) <$> elimUbxSumRhs rhs) bndrs
+
+elimUbxSumRhs :: StgRhs -> UniqSM StgRhs
+elimUbxSumRhs (StgRhsClosure ccs b_info fvs update_flag srt args expr)
+  = StgRhsClosure ccs b_info fvs update_flag srt args <$> elimUbxSumExpr expr
+
+elimUbxSumRhs (StgRhsCon ccs con args)
+  | isUnboxedTupleCon con
+  = uncurry (StgRhsCon ccs) <$> elimUbxConApp con args
+
+  | otherwise
+  = return (StgRhsCon ccs con args)
+
+elimUbxSumExpr :: StgExpr -> UniqSM StgExpr
+elimUbxSumExpr e@StgApp{}
+  = return e
+
+elimUbxSumExpr e@StgLit{}
+  = return e
+
+elimUbxSumExpr e@(StgConApp con args)
+  | isUnboxedSumCon con
+  = uncurry StgConApp <$> elimUbxConApp con args
+
+  | otherwise
+  = return e
+
+elimUbxSumExpr e@StgOpApp{}
+  = return e
+
+elimUbxSumExpr (StgLam args e)
+  = -- this shouldn't happen but whatever
+    StgLam args <$> elimUbxSumExpr e
+
+elimUbxSumExpr case_@(StgCase e case_lives alts_lives bndr srt alt_ty alts)
+  | isUnboxedSumType (idType bndr)
+  , (tycon, _) <- splitTyConApp (idType bndr)
+  = do let (ubx_fields, bx_fields) = tyConFields tycon
+
+       tag_binder <- mkSysLocalM (mkFastString "tag") intPrimTy
+
+       ubx_field_binders <-
+         replicateM ubx_fields (mkSysLocalM (mkFastString "ubx") intPrimTy)
+
+       boxed_field_binders <-
+         replicateM bx_fields (mkSysLocalM (mkFastString "bx") (anyTypeOfKind liftedTypeKind))
+
+       let args = tag_binder : ubx_field_binders ++ boxed_field_binders
+
+       let mkConAlt (DataAlt con, bndrs, useds, rhs) =
+             (LitAlt (MachInt (fromIntegral (dataConTag con))), [], [],
+              undefined)
+
+           mkConAlt alt@(LitAlt{}, _, _, _) =
+             pprPanic "elimUbxSumExpr.mkConAlt" (ppr alt)
+
+       let outer_case =
+             -- TODO: not sure about lives parts
+             StgCase e case_lives alts_lives bndr srt alt_ty
+               [ (DataAlt (tupleDataCon Unboxed (length args)),
+                  args,
+                  replicate (length args) True, -- TODO: fix this
+                  inner_case) ]
+
+           inner_case =
+             StgCase (StgApp tag_binder []) case_lives alts_lives tag_binder srt alt_ty
+               (map mkConAlt alts) -- TODO: we should make sure there's a DEFAULT alt here
+
+       return outer_case
+
+  | otherwise
+  = do e' <- elimUbxSumExpr e
+       alts' <- mapM elimUbxSumAlt alts
+       return (StgCase e' case_lives alts_lives bndr srt alt_ty alts')
+
+elimUbxSumExpr (StgLet bind e)
+  = StgLet <$> elimUbxSum bind <*> elimUbxSumExpr e
+
+elimUbxSumExpr (StgLetNoEscape live_in_let live_in_bind bind e)
+  = StgLetNoEscape live_in_let live_in_bind <$> elimUbxSum bind <*> elimUbxSumExpr e
+
+elimUbxSumExpr (StgTick tick e)
+  = StgTick tick <$> elimUbxSumExpr e
+
+--------------------------------------------------------------------------------
+
+elimUbxSumAlt :: StgAlt -> UniqSM StgAlt
+elimUbxSumAlt (con, xs, uses, e) = (con, xs, uses,) <$> elimUbxSumExpr e
+
+--------------------------------------------------------------------------------
+
+-- elimUbxSumCon :: DataCon -> DataCon
+-- elimUbxSumCon = id
+
+-- | Returns (# unboxed fields, # boxed fields) for a UnboxedSum TyCon.
+tyConFields :: TyCon -> (Int, Int)
+tyConFields tycon
+  = let
+      all_cons         = tyConDataCons tycon
+      cons_rep_arg_tys = map dataConRepArgTys all_cons
+
+      -- fst: prim types
+      -- snd: non-prim types
+      con_rep_tys_parts :: [([Type], [Type])]
+      con_rep_tys_parts = map (partition isPrimitiveType) cons_rep_arg_tys
+
+      fields_unboxed = maximum (0 : map (length . fst) con_rep_tys_parts)
+      fields_boxed   = maximum (0 : map (length . snd) con_rep_tys_parts)
+    in
+      (fields_unboxed, fields_boxed)
+
+--------------------------------------------------------------------------------
+
+-- TODO: We should memoize this somehow, no need to generate same information
+-- for every DataCon of a TyCon.
+--
+-- !!! I think we should memoize this in TycCon (maybe in AlgTyConRhs.SumTyCon) !!!
+elimUbxConApp :: DataCon -> [StgArg] -> UniqSM (DataCon, [StgArg])
+elimUbxConApp con args
+  = let
+      (fields_unboxed, fields_boxed) = tyConFields (dataConTyCon con)
+
+      con_unboxed_args, con_boxed_args :: [StgArg]
+      (con_unboxed_args, con_boxed_args) = partition (isPrimitiveType . stgArgType) args
+
+      tuple_con = tupleDataCon Unboxed (length new_args)
+      tag_arg   = StgLitArg (MachWord (fromIntegral (dataConTag tuple_con)))
+
+      -- FIXME: What to put here in place of undefined?
+      dummy_arg = StgLitArg (MachWord 0)
+
+      unboxed_args =
+        con_unboxed_args ++ replicate (fields_unboxed - length con_unboxed_args) dummy_arg
+      boxed_args   =
+        con_boxed_args ++ replicate (fields_boxed - length con_boxed_args) dummy_arg
+
+      new_args = tag_arg : unboxed_args ++ boxed_args
+    in
+      pprTrace "elimUbxConApp" (ppr (tuple_con, new_args)) $ return (tuple_con, new_args)
