3e00042a47d2ab95e055c70059fae2b3d3a6f657
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-12-14 20:09:11 -0500

    fix some warnings

--- compiler/types/Type.hs
+++ compiler/types/Type.hs
@@ -1097,6 +1097,8 @@ data RepType = UbxTupleRep [UnaryType]
 
 instance Outputable RepType where
   ppr (UbxTupleRep tys) = ptext (sLit "UbxTupleRep") <+> ppr tys
+  ppr (UbxSumRep ubx_tys bx_tys) =
+    ptext (sLit "UbxSumRep") <+> ppr ubx_tys <+> ppr bx_tys
   ppr (UnaryRep ty)     = ptext (sLit "UnaryRep")    <+> ppr ty
 
 flattenRepType :: RepType -> [UnaryType]
