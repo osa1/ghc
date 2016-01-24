115f1009113938154c738c521336383f5b255466
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-01-23 22:29:06 -0500

    big patch: - update unboxedSumTyConFields to handle recursively unpacked sum types - move things around: sum-specific parts moved to ElimUbxSums

--- compiler/simplStg/ElimUbxSums.hs
+++ compiler/simplStg/ElimUbxSums.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE CPP, TupleSections #-}
 
-module ElimUbxSums ( elimUbxSums ) where
+module ElimUbxSums
+  ( elimUbxSums
+  , unboxedSumTyConFields
+  , unboxedSumRepTypes
+  ) where
 
 #include "HsVersions.h"
 
@@ -327,9 +331,54 @@ liftedAny = anyTypeOfKind liftedTypeKind
 
 --------------------------------------------------------------------------------
 
+-- INVARIANT: Returned list doesn't have unboxed tuples or sums.
+unboxedSumRepTypes :: [Type] -> [Type]
+unboxedSumRepTypes alts =
+    let
+      alt_tys = map go alts
+
+      con_rep_tys_parts :: [([Type], [Type])]
+      con_rep_tys_parts = map (partition isPrimitiveType) alt_tys
+
+      fields_unboxed = maximum (0 : map (length . fst) con_rep_tys_parts)
+      fields_boxed   = maximum (0 : map (length . snd) con_rep_tys_parts)
+
+      go ty
+        | Just (tc, args) <- splitTyConApp_maybe ty
+        , isUnboxedTupleTyCon tc
+        = args
+
+        | Just (tc, args) <- splitTyConApp_maybe ty
+        , isUnboxedSumTyCon tc
+        = unboxedSumRepTypes args
+
+        | otherwise
+        = [ty]
+
+      ret = intPrimTy :
+            replicate fields_unboxed intPrimTy ++
+            replicate fields_boxed liftedAny
+    in
+      ASSERT (not (any isUnboxedSumType ret) && not (any isUnboxedTupleType ret))
+      ret
+
+-- | Returns (# unboxed fields, # boxed fields) for a UnboxedSum TyCon
+-- application. NOTE: Tag field is included.
+unboxedSumTyConFields :: [Type] -> (Int, Int)
+unboxedSumTyConFields alts =
+    let
+      rep_tys = unboxedSumRepTypes alts
+      (ubx_tys, bx_tys) = partition isPrimitiveType rep_tys
+    in
+      (length ubx_tys, length bx_tys)
+
 elimUbxConApp :: DataCon -> [StgArg] -> [Type] -> (DataCon, [(Maybe PrimOp, StgArg)])
 elimUbxConApp con stg_args ty_args
   = ASSERT (isUnboxedSumCon con)
+    -- One assumption here is that we only have one argument. The reason is
+    -- because alts of sum types accept only one argument and multiple arguments
+    -- are handled using unboxed tuples.
+    ASSERT (length stg_args == 1)
     -- pprTrace "elimUbxConApp"
     --   (text "con:" <+> ppr con $$
     --    text "stg_args:" <+> ppr stg_args $$
