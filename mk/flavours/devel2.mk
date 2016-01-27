7863d7c232d8c975ae8131c24a10549e488210e9
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-01-27 12:00:25 -0500

    disable extra -DDEBUGs as we're 1.0 now

--- mk/flavours/devel2.mk
+++ mk/flavours/devel2.mk
@@ -1,5 +1,5 @@
-SRC_HC_OPTS        = -O -H64m -DDEBUG
-GhcStage1HcOpts    = -O -DDEBUG
+SRC_HC_OPTS        = -O -H64m
+GhcStage1HcOpts    = -O
 GhcStage2HcOpts    = -O0 -DDEBUG
 GhcLibHcOpts       = -O -dcore-lint
 BUILD_PROF_LIBS    = NO
