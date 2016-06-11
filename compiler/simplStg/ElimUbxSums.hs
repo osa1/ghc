707aeac518e3f193f61b555bd8d41716ead62cd1
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-06-11 08:30:04 -0400

    implement sharing slots

--- compiler/simplStg/ElimUbxSums.hs
+++ compiler/simplStg/ElimUbxSums.hs
@@ -10,6 +10,8 @@
 module ElimUbxSums
   ( mkUbxSumRepTy
   , mkUbxSum
+  , rnUbxSumBndrs
+
   , ubxSumSlots
   , ubxSumRepType
   , UbxSumRepTy
@@ -20,8 +22,8 @@ module ElimUbxSums
 #include "HsVersions.h"
 
 import BasicTypes
+import Id
 import Literal
-import MkCore (rUNTIME_ERROR_ID)
 import Outputable
 import StgSyn
 import TyCon
@@ -31,112 +33,196 @@ import TysWiredIn
 import Util (debugIsOn)
 
 import Data.List (foldl', sort, sortOn)
+import Data.Maybe (mapMaybe)
 
 --------------------------------------------------------------------------------
 
 -- | An unboxed sum is represented as its slots. Includes the tag.
 -- INVARIANT: List is sorted.
-newtype UbxSumRepTy = UbxSumRepTy { ubxSumSlots :: [PrimRep] }
+-- INVARIANT: Slot for the tag is always the first in the list.
+--
+-- FIXME: This is causing problems when we generate fresh identifiers from slots
+-- that's supposed to have different types! Should we maybe keep original types
+-- of fields in the original sum type?
+newtype UbxSumRepTy = UbxSumRepTy { ubxSumSlots :: [SlotTy] }
 
 ubxSumFieldTypes :: UbxSumRepTy -> [Type]
-ubxSumFieldTypes = map primRepType . ubxSumSlots
+ubxSumFieldTypes = map slotTyToType . ubxSumSlots
 
 instance Outputable UbxSumRepTy where
   ppr (UbxSumRepTy slots) = text "UbxSumRepTy" <+> ppr slots
 
--- | Given types of constructor arguments, return the unboxed sum type.
+-- | Given types of constructor arguments, return the unboxed sum rep type.
 mkUbxSumRepTy :: [[Type]] -> UbxSumRepTy
 mkUbxSumRepTy constrs =
   ASSERT( length constrs > 1 ) -- otherwise it isn't a sum type
   let
     combine_alts
-      :: [[PrimRep]]  -- constructors
-      -> [PrimRep]    -- final slots
+      :: [[SlotTy]]  -- slots of constructors
+      -> [SlotTy]    -- final slots
     combine_alts constrs = foldl' constr_reps [] constrs
 
-    constr_reps :: [PrimRep] -> [PrimRep] -> [PrimRep]
-    constr_reps existing_reps [] = existing_reps
-    constr_reps existing_reps (r : rs)
-      | Just existing_reps' <- consume_rep_slot r existing_reps
-      = r : constr_reps existing_reps' rs
-      | otherwise
-      = r : constr_reps existing_reps rs
+    constr_reps :: [SlotTy] -> [SlotTy] -> [SlotTy]
+    constr_reps existing_slots []
+      = existing_slots
+    constr_reps [] needed_slots
+      = needed_slots
+    constr_reps (es : ess) (s : ss)
+      | Just s' <- s `fitsIn` es
+      = -- found a slot, use it
+        s' : constr_reps ess ss
 
-    -- | TODO: document me
-    consume_rep_slot :: PrimRep -> [PrimRep] -> Maybe [PrimRep]
-    consume_rep_slot r (r' : rs)
-      | r == r'
-      = Just rs -- found a slot, use it and return the rest
       | otherwise
-      = (r' :) <$> consume_rep_slot r rs -- keep searching for a slot
-    consume_rep_slot _ []
-      = Nothing -- couldn't find a slot for this rep
+      = -- keep searching for a slot
+        es : constr_reps ess (s : ss)
 
     -- Nesting unboxed tuples and sums is OK, so we need to flatten first.
     constr_flatten_tys :: [Type] -> [Type]
-    constr_flatten_tys fields = concatMap (flattenRepType . repType) fields
+    constr_flatten_tys = concatMap (flattenRepType . repType)
 
-    -- No need to allocate VoidRep slots as those types are simply not available
-    -- in runtime.
-    filter_voids :: [PrimRep] -> [PrimRep]
-    filter_voids = filter (not . isVoidRep)
+    constr_slot_tys :: [Type] -> [SlotTy]
+    constr_slot_tys = sort . mapMaybe typeSlotTy . constr_flatten_tys
 
-    constr_rep_tys :: [Type] -> [PrimRep]
-    constr_rep_tys = filter_voids . map typePrimRep . constr_flatten_tys
+    sumRep = UbxSumRepTy (WordSlot : combine_alts (map constr_slot_tys constrs))
   in
-    UbxSumRepTy (IntRep : sort (combine_alts (map constr_rep_tys constrs)))
+    -- pprTrace "mkUbxSumRepTy" (ppr sumRep) sumRep
+    sumRep
 
-mkUbxSum :: UbxSumRepTy -> ConTag -> [(PrimRep, StgArg)] -> StgExpr
+-- | Build a unboxed sum term.
+mkUbxSum :: UbxSumRepTy -> ConTag -> [(Type, StgArg)] -> StgExpr
 mkUbxSum sumTy tag fields =
   let
-    sorted_fields = sortOn fst fields
+    field_slots = mkSlots fields
 
-    bindFields :: [PrimRep] -> [(PrimRep, StgArg)] -> [StgArg]
-    bindFields reps []
+    bindFields :: [SlotTy] -> [(SlotTy, StgArg)] -> [StgArg]
+    bindFields slots []
       = -- arguments are bound, fill rest of the slots with dummy values
-        map repDummyArg reps
+        map slotDummyArg slots
     bindFields [] args
       = -- we still have arguments to bind, but run out of slots
         pprPanic "mkUbxSum" (text "Run out of slots. Args left to bind:" <+> ppr args)
-    bindFields reps0@(rep : reps) args0@((arg_rep, arg) : args)
-      | rep == arg_rep
-      = arg : bindFields reps args
-      | rep < arg_rep
-      = repDummyArg rep : bindFields reps args0
+    bindFields (slot : slots) args0@((arg_slot, arg) : args)
+      | Just arg_slot == (arg_slot `fitsIn` slot)
+      = arg : bindFields slots args
       | otherwise
-      = pprPanic "mkUbxSum" (text "Can't bind arg with rep" <+> ppr arg_rep $$
-                             text "Slots left:" <+> ppr reps0 $$
-                             text "sum ty:" <+> ppr sumTy $$
-                             text "tag:" <+> ppr tag $$
-                             text "fields:" <+> ppr fields)
+      = slotDummyArg slot : bindFields slots args0
   in
     StgConApp (tupleDataCon Unboxed (length (ubxSumSlots sumTy)))
               (StgLitArg (MachInt (fromIntegral tag))
-                : (bindFields (tail (ubxSumSlots sumTy)) sorted_fields))
+                : (bindFields (tail (ubxSumSlots sumTy)) -- drop tag slot
+                              field_slots))
 
-primRepType :: PrimRep -> Type
-primRepType VoidRep   = voidPrimTy
-primRepType PtrRep    = anyTypeOfKind liftedTypeKind
-primRepType IntRep    = intPrimTy
-primRepType WordRep   = wordPrimTy
-primRepType Int64Rep  = int64PrimTy
-primRepType Word64Rep = word64PrimTy
-primRepType AddrRep   = addrPrimTy
-primRepType FloatRep  = floatPrimTy
-primRepType DoubleRep = doublePrimTy
-primRepType VecRep{}  = error "primRepType: VecRep not supported yet"
+-- | Given binders for an unboxed sum and binders for an alternative, return
+-- renamings of binders of the alternative, so that they point to their slots.
+rnUbxSumBndrs :: [(Type, Id)] -> [(Type, Id)] -> [(Id, Id)]
+rnUbxSumBndrs ubx_sum_bndrs alt_bndrs =
+  let
+    ubx_sum_slots = mkSlots ubx_sum_bndrs
+    alt_slots     = mkSlots alt_bndrs
 
-repDummyArg :: PrimRep -> StgArg
-repDummyArg VoidRep   = error "repDummyArg: VoidRep"
-repDummyArg PtrRep    = StgVarArg rUNTIME_ERROR_ID
-repDummyArg IntRep    = StgLitArg (MachInt 0)
-repDummyArg WordRep   = StgLitArg (MachWord 0)
-repDummyArg Int64Rep  = StgLitArg (MachInt64 0)
-repDummyArg Word64Rep = StgLitArg (MachWord64 0)
-repDummyArg AddrRep   = error "repDummyArg: AddrRep" -- FIXME: Need a MachLabel here...
-repDummyArg FloatRep  = StgLitArg (MachFloat 0.0)
-repDummyArg DoubleRep = StgLitArg (MachDouble 0.0)
-repDummyArg VecRep{}  = error "repDummyArg: Found VecRep"
+    mapBinders :: [(SlotTy, Id)] -> [(SlotTy, Id)] -> [(Id, Id)]
+    mapBinders _ []
+      = []
+    mapBinders [] _
+      = pprPanic "rnUbxSumBndrs.mapBinders"
+          (text "Run out of slots but still have fields to bind." $$ ppr ubx_sum_bndrs $$ ppr alt_bndrs)
+    mapBinders ((sum_slot_ty, sum_slot_id) : sum_slots) alt_ss@((alt_slot_ty, alt_slot_id) : alt_slots)
+      | Just sum_slot_ty == (alt_slot_ty `fitsIn` sum_slot_ty)
+      = (alt_slot_id, sum_slot_id) : mapBinders sum_slots alt_slots
+
+      | otherwise
+      = mapBinders sum_slots alt_ss
+  in
+    mapBinders ubx_sum_slots alt_slots
+
+--------------------------------------------------------------------------------
+
+-- We have 3 kinds of slots:
+--
+--   - 64bit integer slots: Shared between PtrRep, IntRep, WordRep, Int64Rep,
+--     Word64Rep, AddrRep.
+--
+--   - Word-sized integer slots: Shared between PtrRep, IntRep, WordRep,
+--     AddrRep.
+--
+--   - Word-sized float slots: Just FloatRep.
+--
+--   - Double-sized float slots: Shared between FloatRep and DoubleRep.
+--
+-- Here's how we generate slots to use for a given sum type:
+--
+--   - A first pass "canonicalize" reps. For all Word-sized integer reps we
+--     return WordRep, for all Float-sized reps we return FloatRep etc.
+--
+--   - We then sort the slots. Comparison works as follows: Float slots are
+--     always smaller than integer slots. In the same category we compare sizes.
+--     (e.g. DoubleRep is greater than FloatRep)
+
+data SlotTy = WordSlot | Word64Slot | FloatSlot | DoubleSlot
+  deriving (Eq, Ord) -- Constructor order is important!
+
+instance Outputable SlotTy where
+  ppr Word64Slot = text "Word64Slot"
+  ppr WordSlot   = text "WordSlot"
+  ppr DoubleSlot = text "DoubleSlot"
+  ppr FloatSlot  = text "FloatSlot"
+
+-- Some types don't have any slots, e.g. the ones with VoidRep.
+typeSlotTy :: Type -> Maybe SlotTy
+typeSlotTy ty =
+    if isVoidRep primRep then Nothing else Just (primRepSlot primRep)
+  where
+    primRep = typePrimRep ty
+
+mkSlots :: [(Type, a)] -> [(SlotTy, a)]
+mkSlots = reverse . sortOn fst . mapMaybe (\(ty, bndr) -> (,bndr) <$> typeSlotTy ty)
+
+primRepSlot :: PrimRep -> SlotTy
+primRepSlot VoidRep     = pprPanic "primRepSlot" (text "No slot for VoidRep")
+primRepSlot PtrRep      = WordSlot
+primRepSlot IntRep      = WordSlot
+primRepSlot WordRep     = WordSlot
+primRepSlot Int64Rep    = Word64Slot
+primRepSlot Word64Rep   = Word64Slot
+primRepSlot AddrRep     = WordSlot
+primRepSlot FloatRep    = FloatSlot
+primRepSlot DoubleRep   = DoubleSlot
+primRepSlot VecRep{}    = pprPanic "primRepSlot" (text "No slot for VecRep")
+
+slotTyToType :: SlotTy -> Type
+slotTyToType Word64Slot = word64PrimTy
+slotTyToType WordSlot   = wordPrimTy
+slotTyToType DoubleSlot = doublePrimTy
+slotTyToType FloatSlot  = floatPrimTy
+
+isWordSlot :: SlotTy -> Bool
+isWordSlot Word64Slot = True
+isWordSlot WordSlot   = True
+isWordSlot _          = False
+
+isFloatSlot :: SlotTy -> Bool
+isFloatSlot DoubleSlot = True
+isFloatSlot FloatSlot  = True
+isFloatSlot _          = False
+
+fitsIn :: SlotTy -> SlotTy -> Maybe SlotTy
+fitsIn ty1 ty2
+  | isWordSlot ty1 && isWordSlot ty2
+  = Just (max ty1 ty2)
+
+  | isFloatSlot ty1 && isFloatSlot ty2
+  = Just (max ty1 ty2)
+
+  | otherwise
+  = Nothing
+
+--------------------------------------------------------------------------------
+
+slotDummyArg :: SlotTy -> StgArg
+slotDummyArg Word64Slot = StgLitArg (MachWord64 0)
+slotDummyArg WordSlot   = StgLitArg (MachWord 0)
+slotDummyArg DoubleSlot = StgLitArg (MachDouble 0.0)
+slotDummyArg FloatSlot  = StgLitArg (MachFloat 0.0)
 
 --------------------------------------------------------------------------------
 
@@ -144,4 +230,4 @@ ubxSumRepType :: [Type] -> RepType
 ubxSumRepType = UbxSumRep . mkUbxSumRepTy . map return
 
 flattenSumRep :: UbxSumRepTy -> [UnaryType]
-flattenSumRep = map primRepType . ubxSumSlots
+flattenSumRep = map slotTyToType . ubxSumSlots
--- compiler/types/TyCon.hs
+++ compiler/types/TyCon.hs
@@ -1199,7 +1199,7 @@ data PrimRep
   | FloatRep
   | DoubleRep
   | VecRep Int PrimElemRep  -- ^ A vector
-  deriving( Eq, Show, Ord )
+  deriving( Eq, Show )
 
 data PrimElemRep
   = Int8ElemRep
@@ -1212,7 +1212,7 @@ data PrimElemRep
   | Word64ElemRep
   | FloatElemRep
   | DoubleElemRep
-   deriving( Eq, Show, Ord )
+   deriving( Eq, Show )
 
 instance Outputable PrimRep where
   ppr r = text (show r)
